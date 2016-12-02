9b60d252985368a050d6db06cbca00cc9634232a
Andrew Hoefling - terra <andrew@hoeflingsoftware.com>
2016-12-02 00:20:01 -0500

    removing everything

--- FireWaterProject/WaterfallDemo/WaterFallDemo.js
+++
@@ -1,9 +0,0 @@
-﻿var game;
-var worldLocation = "Models/island_under_falls/island_under_falls.dae";
-var particleLocation = "Models/metal_sphere.dae";
-c3dl.addModel(worldLocation);
-c3dl.addModel(particleLocation);
-function startScene() {
-    game = new GameController("worldDemo", worldLocation);
-    game.canvasMain();
-}
